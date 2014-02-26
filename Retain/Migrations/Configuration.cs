cd6062b85e8c177ce6936148a12240e62b6f873c
Russ Frisch <rfrisch@alinean.com>
2014-02-25 23:24:28 -0500

    Back to automatic migrations

--- Retain/Migrations/Configuration.cs
+++ Retain/Migrations/Configuration.cs
@@ -9,7 +9,8 @@ namespace Retain.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Retain.Models.ApplicationDbContext context)
